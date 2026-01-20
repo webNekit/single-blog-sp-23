83cbd6483e37852dea24e2a37cc348dcdc5c35eb
webNekit <mochalov-hunter@mail.ru>
2026-01-20 19:50:12 +0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { envConfig } from './common/config/env.config';
+import * as path from 'path';
+import * as process from 'node:process';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envConfig.validationSchema,
+    }),
+    ServeStaticModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const uploadDir = configService.get<string>('UPLOAD_DIR') || 'uploads';
+        const absolutePath = path.join(process.cwd(), uploadDir);
+
+        return [
+          {
+            rootPath: absolutePath,
+            serveRoot: '/uploads',
+            serveStaticOptions: { index: false },
+          },
+        ];
+      },
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.enableCors({
+    origin: configService.get<string>('CORS_ORIGIN') || '*',
+    credentials: true,
+  });
+  const port = configService.get<number>('PORT') || 9000;
+  await app.listen(port);
 }
 bootstrap();
